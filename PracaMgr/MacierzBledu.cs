a04a856672eab9a78d445f11e6fe8faa4cf98995
Krzysztof <wisnia18pl@gmail.com>
2019-01-08 23:02:16 +0100

    zmiany_klas

---
+++ PracaMgr/MacierzBledu.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime;
+
+namespace PracaMgr
+{
+    public class MecierzBledu
+    {
+        private double[] miary;
+        private List<int> decyzje;
+        private double truePositive;
+        private double falsePositive;
+        private double trueNegative;
+        private double falseNegative;
+
+        public MecierzBledu(double[] Miary, List<int> decyzje)
+        {
+            this.miary = Miary;
+            this.decyzje = decyzje;
+        }
+
+        public double wyliczAcc()
+        {
+            double wynik = (this.truePositive + this.trueNegative) / (this.truePositive + this.trueNegative + this.falsePositive + this.falseNegative);
+            return Math.Round(wynik, 2, MidpointRounding.AwayFromZero);
+        }
+
+        public void wyznaczMacierzBledu(double Lambda)
+        {
+            zerujMacierzBledu();
+            bool flaga = false;
+            for (int i = 0; i < this.miary.Length; i++)
+            {
+                flaga = (this.miary[i] >= Lambda);
+
+                if (flaga)
+                {
+                    if (decyzje[i] == 1)
+                        this.truePositive++;
+                    else
+                        this.falsePositive++;
+                }
+                else
+                {
+                    if (decyzje[i] == 0)
+                        this.trueNegative++;
+                    else
+                        this.falseNegative++;
+                }
+            }
+        }
+
+        private void zerujMacierzBledu()
+        {
+            this.truePositive = 0;
+            this.falsePositive = 0;
+            this.trueNegative = 0;
+            this.falseNegative = 0;
+        }
+    }
+}
--- PracaMgr/OknoGlowne.cs
+++ PracaMgr/OknoGlowne.cs
@@ -18,13 +18,7 @@ namespace PracaMgr
         List<int> Decyzje = new List<int>();
         public OknoGlowne()
         {
-            InitializeComponent();
-            
-        }
-
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
-        {
-
+            InitializeComponent();    
         }
 
         private void btnWczytajPlik_Click(object sender, EventArgs e)
@@ -67,37 +61,12 @@ namespace PracaMgr
 
         private void btnOblicz_Click(object sender, EventArgs e)
         {
-            List<double[]> wynik = new List<double[]>();
-            float Lambda = 0;
-            List<double[]> miary = generujMiary();
-            for (float i = 0; i < 11; i++)
-            {
-                double[] wynikMiar = new double[Obiekty[1].Length];
-                Lambda = i / 10;
-
-                for (int j = 0; j < miary.Count; j++)
-                {
-                    wynikMiar[j] = WyznaczWartoscMiary(Lambda, miary[j]);
-                }
-             wynik.Add(wynikMiar);
-             //MessageBox.Show(string.Join(",", wynikMiar));
-            }
-            string aaa = "";
-            for (int i = 0; i < wynik.Count; i++)
-            {
-                for (int j = 0; j < wynik[1].Length; j++)
-                {
-                    aaa = aaa + wynik[i][j].ToString("0.00") + " | ";  
-                }
-                aaa = aaa + "newline";
-            }
-            Clipboard.SetText(aaa);
-            MessageBox.Show(aaa);
+            generujZestawienie();
         }
 
-        private List<double[]> generujMiary()
+        private List<MecierzBledu> generujMiary()
         {
-            List<double[]> miary = new List<double[]>();
+            List<MecierzBledu> miary = new List<MecierzBledu>();
             double[] miara = new double[Decyzje.Count];
             for (int i = 0; i < Obiekty.First().Length; i++)
             {
@@ -106,24 +75,44 @@ namespace PracaMgr
                 {
                     miara[k] = Obiekty[k][i];
                 }
-                miary.Add(miara);
+                miary.Add(new MecierzBledu(miara,Decyzje));
             }
 
             return miary;
         }
 
-        private double WyznaczWartoscMiary(double Lambda, double[] obiekty)
+        private void generujZestawienie()
         {
-            ConvMatrix macierz = new ConvMatrix(obiekty, Decyzje, Lambda);
-            return macierz.wyliczAcc();
-        }
+            double Lambda = 0;
+            List<double[]> wynik = new List<double[]>();
+            List<MecierzBledu> miary = generujMiary();
 
-        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (!char.IsNumber(e.KeyChar) && e.KeyChar != ',')
+            for (float i = 0; i < 11; i++)
             {
-                e.Handled = e.KeyChar != (char)Keys.Back;
+                double[] wynikMiar = new double[Obiekty[1].Length];
+                Lambda = Math.Round(i/10,2);
+
+                for (int j = 0; j < miary.Count; j++)
+                {
+                    miary[j].wyznaczMacierzBledu(Lambda);
+                    wynikMiar[j] = miary[j].wyliczAcc();
+                }
+                wynik.Add(wynikMiar);
             }
+
+            //to do wywalenia
+            string aaa = "";
+            for (int i = 0; i < wynik.Count; i++)
+            {
+                for (int j = 0; j < wynik[1].Length; j++)
+                {
+                    aaa = aaa + wynik[i][j].ToString("0.00") + " | ";
+                }
+                aaa = aaa + "newline";
+            }
+            Clipboard.SetText(aaa);
+            MessageBox.Show(aaa);
+            //koniec
         }
 
     }
