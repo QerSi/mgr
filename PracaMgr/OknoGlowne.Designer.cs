fe1d87fe8d94279a182a444e8a2cdd6a17a5545d
Krzysztof <wisnia18pl@gmail.com>
2019-01-10 19:33:24 +0100

    front

--- PracaMgr/OknoGlowne.Designer.cs
+++ PracaMgr/OknoGlowne.Designer.cs
@@ -28,15 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OknoGlowne));
             this.btnWczytajPlik = new System.Windows.Forms.Button();
             this.txtSciezka = new System.Windows.Forms.TextBox();
             this.opfWczytaj = new System.Windows.Forms.OpenFileDialog();
             this.btnOblicz = new System.Windows.Forms.Button();
+            this.txtDecyzje = new System.Windows.Forms.TextBox();
+            this.btDecyzje = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnWczytajPlik
             // 
-            this.btnWczytajPlik.Location = new System.Drawing.Point(274, 35);
+            this.btnWczytajPlik.Location = new System.Drawing.Point(288, 199);
             this.btnWczytajPlik.Name = "btnWczytajPlik";
             this.btnWczytajPlik.Size = new System.Drawing.Size(75, 23);
             this.btnWczytajPlik.TabIndex = 0;
@@ -46,7 +59,7 @@
             // 
             // txtSciezka
             // 
-            this.txtSciezka.Location = new System.Drawing.Point(12, 37);
+            this.txtSciezka.Location = new System.Drawing.Point(12, 201);
             this.txtSciezka.Name = "txtSciezka";
             this.txtSciezka.Size = new System.Drawing.Size(256, 20);
             this.txtSciezka.TabIndex = 1;
@@ -57,24 +70,131 @@
             // 
             // btnOblicz
             // 
-            this.btnOblicz.Location = new System.Drawing.Point(132, 155);
+            this.btnOblicz.Location = new System.Drawing.Point(12, 15);
             this.btnOblicz.Name = "btnOblicz";
-            this.btnOblicz.Size = new System.Drawing.Size(75, 23);
+            this.btnOblicz.Size = new System.Drawing.Size(362, 23);
             this.btnOblicz.TabIndex = 4;
             this.btnOblicz.Text = "Oblicz";
             this.btnOblicz.UseVisualStyleBackColor = true;
             this.btnOblicz.Click += new System.EventHandler(this.btnOblicz_Click);
             // 
+            // txtDecyzje
+            // 
+            this.txtDecyzje.Location = new System.Drawing.Point(12, 120);
+            this.txtDecyzje.Name = "txtDecyzje";
+            this.txtDecyzje.Size = new System.Drawing.Size(255, 20);
+            this.txtDecyzje.TabIndex = 5;
+            // 
+            // btDecyzje
+            // 
+            this.btDecyzje.Location = new System.Drawing.Point(288, 118);
+            this.btDecyzje.Name = "btDecyzje";
+            this.btDecyzje.Size = new System.Drawing.Size(75, 23);
+            this.btDecyzje.TabIndex = 6;
+            this.btDecyzje.Text = "Wczytaj";
+            this.btDecyzje.UseVisualStyleBackColor = true;
+            this.btDecyzje.Click += new System.EventHandler(this.btDecyzje_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bodoni MT Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Miary zawierania";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bodoni MT Black", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label2.Location = new System.Drawing.Point(12, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Decyzje";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(32)))), ((int)(((byte)(96)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(386, 87);
+            this.panel1.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Enabled = false;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(132, 76);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(132, 8);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(231, 78);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Ocena\r\nKlasyfikacji";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(32)))), ((int)(((byte)(96)))));
+            this.panel2.Location = new System.Drawing.Point(0, 160);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(385, 2);
+            this.panel2.TabIndex = 10;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(96)))), ((int)(((byte)(32)))), ((int)(((byte)(96)))));
+            this.panel3.Controls.Add(this.btnOblicz);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 266);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(386, 50);
+            this.panel3.TabIndex = 11;
+            // 
             // OknoGlowne
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(385, 304);
-            this.Controls.Add(this.btnOblicz);
-            this.Controls.Add(this.txtSciezka);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(53)))), ((int)(((byte)(65)))));
+            this.ClientSize = new System.Drawing.Size(386, 316);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.btnWczytajPlik);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.btDecyzje);
+            this.Controls.Add(this.txtSciezka);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtDecyzje);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(401, 39);
             this.Name = "OknoGlowne";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ocena Klasyfikacji";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,6 +206,15 @@
         private System.Windows.Forms.TextBox txtSciezka;
         private System.Windows.Forms.OpenFileDialog opfWczytaj;
         private System.Windows.Forms.Button btnOblicz;
+        private System.Windows.Forms.TextBox txtDecyzje;
+        private System.Windows.Forms.Button btDecyzje;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
     }
 }
 
--- PracaMgr/OknoGlowne.cs
+++ PracaMgr/OknoGlowne.cs
@@ -32,12 +32,11 @@ namespace PracaMgr
                 {
                     txtSciezka.Text = opfWczytaj.FileName;
                     Obiekty.Clear();
-                    Decyzje.Clear();
                     WczytajDane(opfWczytaj.FileName);
                 }
                 catch
                 {
-                    MessageBox.Show("Bład odczytu pliku");
+                    MessageBox.Show("Bład wczytania danych");
                     txtSciezka.Text = "";
                 }
             }
@@ -49,19 +48,46 @@ namespace PracaMgr
             while(!obiekty.EndOfStream)
             {
                 string[] v_wiersz = obiekty.ReadLine().Split(';');
-                double[] v_arg = new double[v_wiersz.Length-1];
-                for (int i = 0; i < v_wiersz.Length-1; i++)
+                double[] v_arg = new double[v_wiersz.Length];
+                for (int i = 0; i < v_wiersz.Length; i++)
                 {
-                    v_arg[i] = (double.Parse(v_wiersz[i])); 
+                    v_arg[i] = (double.Parse(v_wiersz[i]));
                 }
                 Obiekty.Add(v_arg);
+            }
+        }
+
+        private void WczytajDecyzje(string p_sciezka)
+        {
+            StreamReader obiekty = new StreamReader(p_sciezka);
+            while (!obiekty.EndOfStream)
+            {
+                string[] v_wiersz = obiekty.ReadLine().Split(';');
                 Decyzje.Add(int.Parse(v_wiersz.Last()));
             }
         }
 
         private void btnOblicz_Click(object sender, EventArgs e)
         {
-            generujZestawienie();
+            string blad = walidujDane();
+            if (blad == "")
+                generujZestawienie();
+            else
+                MessageBox.Show(blad);
+        }
+
+        private string walidujDane()
+        {
+            string blad = "";
+            if (Decyzje.Count != Obiekty.Count)
+                blad = "Nieprawidłowe dane";
+            if (Decyzje.Count == 0)
+                blad = "Brak decyzji";
+            if (Obiekty.Count == 0)
+                blad = "Brak macierzy przynależności";
+
+
+            return blad;
         }
 
         private List<MecierzBledu> generujMiary()
@@ -119,17 +145,25 @@ namespace PracaMgr
                 sb.AppendLine(str);
             }
 
-            SaveFileDialog dlg = new SaveFileDialog();
-            dlg.FileName = "wynik";
-            dlg.DefaultExt = "csv";
-            dlg.ValidateNames = true;
-
-            dlg.Filter = "CSV (.csv)|*.csv";
-
-            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            try
             {
-                File.WriteAllText(dlg.FileName, sb.ToString());
+                SaveFileDialog dlg = new SaveFileDialog();
+                dlg.FileName = "wynik";
+                dlg.DefaultExt = "csv";
+                dlg.ValidateNames = true;
+
+                dlg.Filter = "CSV (.csv)|*.csv";
+
+                if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    File.WriteAllText(dlg.FileName, sb.ToString());
+                }
             }
+            catch
+            {
+                MessageBox.Show("Bład zapisu pliku");
+            }
+
         }
 
         private string generujNazwyKolumn(int liczbaKolumn)
@@ -142,5 +176,30 @@ namespace PracaMgr
             return wynik;
         }
 
+        private void btDecyzje_Click(object sender, EventArgs e)
+        {
+            opfWczytaj.Filter = "CSV|*.csv";
+            opfWczytaj.Title = "Wybierz plik";
+
+            if (opfWczytaj.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                try
+                {
+                    txtDecyzje.Text = opfWczytaj.FileName;
+                    Decyzje.Clear();
+                    WczytajDecyzje(opfWczytaj.FileName);
+                }
+                catch
+                {
+                    MessageBox.Show("Bład wczytania decyzji");
+                    txtDecyzje.Text = "";
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
